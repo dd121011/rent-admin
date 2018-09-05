95dba54365de6961aeb94c4a040e1c7f896e4928
Ryan <benny_mouyi@163.com>
2018-09-05 22:54:10 +0800

    update release.sh

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sed 's/active: dev/active: release/g' src/main/resources/application.yml > src/main/resources/.application.yml.tmp
+mv src/main/resources/.application.yml.tmp src/main/resources/application.yml
+
+echo '[package]'
+
+mvn clean package
+
+sed 's/active: release/active: dev/g' src/main/resources/application.yml > src/main/resources/.application.yml.tmp
+mv src/main/resources/.application.yml.tmp src/main/resources/application.yml
+
+echo '[sending]'
+
+scp target/rent-admin.jar root@47.106.212.7:/root/project/RentAdmin/
+
+echo '[restarting]'
+
+ssh root@47.106.212.7 "sh /root/project/RentAdmin/restart.sh"
+
+echo '[done]'
+
+# remove script [restart.sh] :
+# ps aux | grep rent | grep java | awk '{print $2}' | xargs kill -9
+# sleep 5
+# nohup java -jar /root/project/RentAdmin/rent-admin.jar > /root/project/RentAdmin/rent-admin.log &
