79f50450bdf1b3aef3e26908927d8c053b4f8f12
Ryan <benny_mouyi@163.com>
2018-09-07 15:21:53 +0800

    optimize like

--- src/main/java/com/scrats/rent/admin/mapper/UserMapper.java
+++ src/main/java/com/scrats/rent/admin/mapper/UserMapper.java
@@ -22,17 +22,16 @@ public interface UserMapper extends BaseMapper<User> {
     @Select("<script>select u.* " +
             "from user u " +
             "where 1=1 " +
-            "<if test='user.userId != null'>and r.user_id = #{user.userId}</if>" +
-            "<if test='user.name != null and user.name !=\"\"'>and r.name like '%'||#{user.name}||'%'</if>" +
-            "<if test='user.qq != null and user.qq !=\"\"'>and r.qq like '%'||#{user.qq}||'%'</if>" +
-            "<if test='user.wechat != null and user.wechat !=\"\"'>and r.wechat like '%'||#{user.wechat}||'%'</if>" +
-            "<if test='user.email != null and user.email !=\"\"'>and r.email like '%'||#{user.email}||'%'</if>" +
-            "<if test='user.profession != null and user.profession !=\"\"'>and r.profession like '%'||#{user.profession}||'%'</if>" +
-            "<if test='user.hometown != null and user.hometown !=\"\"'>and r.hometown like '%'||#{user.hometown}||'%'</if>" +
-            "<if test='user.phone != null and user.phone !=\"\"'>and r.phone like '%'||#{user.phone}||'%'</if>" +
-            "<if test='user.sex != null and user.sex !=\"\"'>and r.sex = #{user.sex}</if>" +
-            "<if test='user.userId != null'>and r.user_id = #{user.userId}</if>" +
-            "<if test='user.deleteTs != null and user.deleteTs > 0'>and r.delete_ts > 0 and check_ts > 0</if>" +
+            "<if test='user.userId != null'>and u.user_id = #{user.userId}</if>" +
+            "<if test='user.name != null and user.name !=\"\"'>and u.name like concat('%', #{user.name}, '%')</if>" +
+            "<if test='user.phone != null and user.phone !=\"\"'>and u.phone like concat('%', #{user.phone}, '%')</if>" +
+            "<if test='user.qq != null and user.qq !=\"\"'>and u.qq like concat('%', #{user.qq}, '%')</if>" +
+            "<if test='user.wechat != null and user.wechat !=\"\"'>and u.wechat like concat('%', #{user.wechat}, '%')</if>" +
+            "<if test='user.email != null and user.email !=\"\"'>and u.email like concat('%', #{user.email}, '%')</if>" +
+            "<if test='user.profession != null and user.profession !=\"\"'>and u.profession like concat('%', #{user.profession}, '%')</if>" +
+            "<if test='user.hometown != null and user.hometown !=\"\"'>and u.hometown like concat('%', #{user.hometown}, '%')</if>" +
+            "<if test='user.sex != null and user.sex !=\"\"'>and u.sex = #{user.sex}</if>" +
+            "<if test='user.deleteTs != null and user.deleteTs > 0'>and u.delete_ts > 0 and check_ts > 0</if>" +
             "<if test='user.deleteTs == null or user.deleteTs == 0'>and (check_ts = 0 or (check_ts > 0 and delete_ts = 0))</if>" +
             "<if test='user.checkTs != null or user.checkTs == 0'>and checkTs = 0</if>" +
             "<if test='user.checkTs != null or user.checkTs > 0'>and checkTs > 0</if>" +
